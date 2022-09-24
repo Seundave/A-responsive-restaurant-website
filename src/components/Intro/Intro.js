00660f1bd5d92a556704943b77e4f77c07216ed2
David <olayinkadavid62@yahoo.com>
2022-09-24 05:58:55 +0100

    Removed video autoplay

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,32 +1,33 @@
 import React from 'react'
 import './Intro.css'
-// import {BsFillPlayFill,BsPauseFill} from 'react-icons/bs'
+import {BsFillPlayFill,BsPauseFill} from 'react-icons/bs'
 import meal from '../../assets/meal.mp4'
 
 function Intro() {
-    // const [playVideo, setPlayVideo] = React.useState(false)
+    const [playVideo, setPlayVideo] = React.useState(false);
     const vidRef = React.useRef();
 
-    // const handleVideo =()=>{
-    //     setPlayVideo ((prevPlayVideo)=> !prevPlayVideo);
 
-    //     if (playVideo){
-    //         vidRef.current.pause();
-    //     }else{
-    //         vidRef.current.play();
-    //     }
-    // }
+    const handleVideo =()=>{
+        setPlayVideo ((prevPlayVideo)=> !prevPlayVideo);
+
+        if (playVideo){
+            vidRef.current.pause();
+        }else{
+            vidRef.current.play();
+        }
+    }
   return (
     <div className='intro-video'>
-        <video src={meal} ref={vidRef} type="video/mp4" loop muted autoPlay="autoplay"/>
+        <video src={meal} ref={vidRef} type="video/mp4" Loop controls={false} muted/>
 
-        {/* <div className='video-overlay'>
-            <div className="video-overlay-circle" onclick={handleVideo}>
+        <div className='video-overlay'>
+            <div className="video-overlay-circle" onClick={handleVideo}>
                 {playVideo 
-                ? <BsPauseFill color="#fff" font-size={30}/>
+                ? <BsPauseFill color="#fff" font-size={30} />
                 : <BsFillPlayFill color="#fff" font-size={30}/>}
             </div>
-        </div> */}
+        </div>
     </div>
   )
 }
